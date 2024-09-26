1559995729682041a23fa4419e739b750cdc8dc4
Olaf <olaf.chuszno@gmail.com>
2024-09-26 14:06:32 +0200

    Further refactor and use template

--- refactored_code.php
+++ refactored_code.php
@@ -1,84 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<!-- What is db_bgcolor? Not provided in the email -->
+<body
+  bgcolor="<?php echo $dg_bgcolor ?>"
+>
+  <br>
+
+  <table width=95%>
+    <?php echo $result ?>
+  </table>
+</body>
+
+</html>
+
 <?php
 
-// contracts
+$sql_orderby = $_GET['sort'] === 1 ? "2, 4 DESC" : "10";
 
-// 0 => id,
-// 2 => nazwa przedsiebiorcy,
-// 4 => NIP,
-// 10 => kwota,
+// Where condition
+$sql_condition = "id = {$_GET['id']} AND kwota > 10";
 
-if ($_GET['akcja'] == 5) {
+// Both combined
+$orderBy = $_GET['akcja'] === 5 ? "$sql_orderby" : 'id';
 
-    // show contracts with amount more than 10
+$sql_query = mysql_query("SELECT * FROM contracts WHERE $sql_condition ORDER BY $orderBy");
 
-    $x = "id = {$_GET[i]} AND kwota > 10; ";
+$result = '';
 
-    $sql_orderby = " order by ";
-
-    switch ($_GET['sort']) {
-      case 1:
-        $sql_orderby.= "2, 4";
-        $b = 'DESC';
-        break;
-      case 2:
-        $sql_orderby.= "10";
-      break;
-    }
-
-    $i = "SELECT * FROM contracts WHERE $x ORDER BY $sql_orderby $b";
-
-    $a = mysql_query($i);
-
-
-    echo `
-    <html>
-      <body bgcolor=$dg_bgcolor>
-        <br>
-        <table width=95%>`;
-
-    while ($z = mysql_fetch_array($a)) {
-
-        echo "
-          <tr>
-            <td>.$z[0]</td>
-            <td>$z[2]
-        ";
-
-        if ($z[10] > 5) {
-          echo " $z[10]";
-        }
-        
-        echo "
-            </td>
-          <tr>
-        ";
-    }
-
-} else {
-
-    $c = mysql_query("SELECT * FROM contracts WHERE $x ORDER BY id");
-
-    echo "
-    <html>
-      <body bgcolor=$dg_bgcolor>
-        <br>
-        <table width=95%>
-    ";
-
-    while ($z = mysql_fetch_array($c)) {
-
-        echo "
-          <tr>
-            <td>$z[0]</td>
-            <td>$z[2]</td>
-          <tr>
-        ";
-    }
-
-}
-
-echo "
-      </table>
-    </body>
-  </html>
+foreach (mysql_fetch_array($sql_query) as $element) {
+  $result.="
+    <tr>
+      <td>.$z[0]</td>
+      <td>$z[2]
   ";
+
+  if ($_GET['akcja'] == 5 && $z[10] > 5) {
+    $result.=" $z[10]";
+  }
+
+  $result.="
+      </td>
+    <tr>
+  ";
+}
